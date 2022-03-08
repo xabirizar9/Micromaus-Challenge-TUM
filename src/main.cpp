cb4219ecfaed04e8202fe56a9c0f77940a1f30dc
Xabier Irizar <xabier.irizar@tum.de>
2022-03-08 15:43:28 +0100

    navigate func

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,15 @@ void print_sensor_data(void *pvParameter)
     }
 }
 
+void navigate(void *pvParameter)
+{
+    while (true)
+    {
+        ESP_LOGD(TAG, "navigating");
+        vTaskDelay(20 / portTICK_PERIOD_MS);
+    };
+};
+
 void main_task(void *pvParameter)
 {
     while (true)
@@ -37,9 +46,11 @@ extern "C" void app_main()
 {
     // Serial.begin(115200, 0);
     // esp_log_level_set(LOG_DEFAULT_LEVEL_VERBOSE);
-    SerialBluetooth::begin("test");
+    // SerialBluetooth::begin("test");
 
-    xTaskCreate(&main_task, "main_task", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
+    // xTaskCreate(&main_task, "main_task", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
 
     xTaskCreate(&print_sensor_data, "print_sensor_data", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
+
+    xTaskCreate(&navigate, "path_navigation", 512, NULL, 5, NULL);
 }
