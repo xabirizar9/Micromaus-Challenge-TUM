d7f84709ca024df1cdd795719a92447f72db76a5
Alexander Fuchs <alex.fu27@gmail.com>
2022-03-10 11:03:03 +0100

    more spi things

--- include/periph/ICM20948.hpp
+++ include/periph/ICM20948.hpp
@@ -1,18 +1,18 @@
 #pragma once
 
 #include <stdint.h>
-#include "support/I2C.hpp"
+#include "support/SPI.hpp"
 
 class ICM20948
 {
 public:
-	ICM20948(uint8_t sclPin, uint8_t mosiPin, uint8_t misoPin);
+	ICM20948(SPIBus& bus, uint8_t csPin);
 	~ICM20948();
 
 protected:
 	void switchBank(uint8_t bank);
 
 private:
-	I2CMaster i2c;
+	SPIDevice spi;
 };
 
