485ac58228e938d9170b0a5730c1f10e6d435db1
Alexander Fuchs <alex.fu27@gmail.com>
2022-03-11 08:34:53 +0100

    (wip) added IMU init functions

--- include/periph/ICM20948.hpp
+++ include/periph/ICM20948.hpp
@@ -9,10 +9,15 @@ public:
 	ICM20948(SPIBus& bus, uint8_t csPin);
 	~ICM20948();
 
-protected:
-	void switchBank(uint8_t bank);
+	/* passing true puts device to sleep
+	 * the wake-up will take max. 40 ms according to datasheet
+	 */
+	void setSleep(bool sleep);
 
 private:
+	void switchBank(uint8_t bank);
+	void init();
+
 	SPIDevice spi;
 };
 
--- src/periph/ICM20948.cpp
+++ src/periph/ICM20948.cpp
@@ -2,6 +2,7 @@
 
 #include "support/I2C.hpp"
 #include <esp_log.h>
+#include <freertos/task.h>
 
 static const char* TAG = "icm";
 
@@ -9,6 +10,7 @@ ICM20948::ICM20948(SPIBus& bus, uint8_t csPin):
 	// todo set higher freq
 	spi(SPIDevice(bus, csPin, 100'000, 0b00))
 {
+	init();
 }
 
 ICM20948::~ICM20948()
@@ -18,4 +20,28 @@ ICM20948::~ICM20948()
 
 void ICM20948::switchBank(uint8_t bank)
 {
+	spi.write8(0x7f, bank);
+}
+
+void ICM20948::setSleep(bool sleep)
+{
+	switchBank(0);
+	// also sets clock to internal
+	spi.write8(0x06, 0x01 | (sleep ? 0x40 : 0x00));
+}
+
+void ICM20948::init()
+{
+	switchBank(0);
+	spi.write8(0x06, 0x80); // reset
+	vTaskDelay(100 / portTICK_PERIOD_MS);
+
+	uint8_t id = spi.read8(0x00);
+	if (id != 0xea) {
+		ESP_LOGW(TAG, "ICM module reports wrong id %02hhx", id);
+	}
+
+	spi.write8(0x03, 0x30); // DMP disabled, fifo disabled, i2c master enabled, SPI mode.
+	setSleep(false); // power up the sensors
+
 }
