11c07d5e89a83490a7eb4a9f5719bd460e327155
Alexander Fuchs <alex.fu27@gmail.com>
2022-03-13 18:33:03 +0100

    added mathematical operations to linalg::Vec

--- include/support/Linalg.hpp
+++ include/support/Linalg.hpp
@@ -2,9 +2,11 @@
 
 #include <cstdint>
 #include <array>
+#include <type_traits>
 
 namespace linalg
 {
+
 template <typename T>
 struct Vec
 {
@@ -17,4 +19,37 @@ struct Vec
 		};
 	};
 };
+
+template<typename T, typename S>
+Vec<std::common_type_t<T, S>> operator*(const Vec<T>& v, const S& t) {
+	return {v.x * t, v.y * t, v.z * t};
 }
+
+template<typename T, typename S>
+Vec<std::common_type_t<T, S>> operator*(const S& t, const Vec<T>& v) {
+	return v * t;
+}
+
+template<typename T, typename S>
+Vec<std::common_type_t<T, S>> operator+(const Vec<T>& v, const Vec<S>& w) {
+	return {v.x + w.x, v.y + w.y, v.z + w.z};
+}
+
+template<typename T, typename S>
+Vec<std::common_type_t<T, S>> operator-(const Vec<T>& v, const Vec<S>& w) {
+	return {v.x - w.x, v.y - w.y, v.z - w.z};
+}
+
+template<typename T, typename S>
+Vec<std::common_type_t<T, S>> operator/(const Vec<T>& v, const S& t) {
+	return {v.x / t, v.y / t, v.z / t};
+}
+
+}
+
+/*
+template<typename T>
+linalg::Vec<T> operator+(const linalg::Vec<T>& v, const linalg::Vec<T>& w) {
+	return {v.x + w.x, v.y + w.y, v.z + w.z};
+}
+*/
