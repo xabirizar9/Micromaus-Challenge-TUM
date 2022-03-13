f44de19491d32c8aa78d36603feea788e72a17cd
wlawlawlad <wlad.meixner@tum.de>
2022-03-14 00:26:12 +0100

    wip: proto regeneration script

---
+++ scripts/gen-pb.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# gen nanopb files
+python3 .pio/libdeps/esp32-wlad-dev/Nanopb/generator/nanopb_generator.py proto/message.proto --strip-path
+mv ./proto/message.pb.c ./src/message.pb.c
+mv ./proto/message.pb.h ./include/message.pb.h
+
+# gen go files
+protoc -I=./proto --go_out=controller ./proto/message.proto
