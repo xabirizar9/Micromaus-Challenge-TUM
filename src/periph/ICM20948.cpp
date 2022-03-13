00c7dba6ac13b7dd9a6fd060e355199c5de8bca5
Alexander Fuchs <alex.fu27@gmail.com>
2022-03-13 14:35:57 +0100

    fixup! (untested) added acceleration reading

--- src/periph/ICM20948.cpp
+++ src/periph/ICM20948.cpp
@@ -25,7 +25,7 @@ ICM20948::~ICM20948()
 void ICM20948::switchBank(uint8_t bank)
 {
 	if (currentBank != bank) {
-		spi.write<uint8_t>(0x7f, bank);
+		spi.write<uint8_t>(0x7f, bank << 4);
 		currentBank = bank;
 	}
 }
@@ -34,21 +34,21 @@ void ICM20948::setSleep(bool sleep)
 {
 	switchBank(0);
 	// also sets clock to internal
-	spi.write<uint8_t>(0x06, 0x01 | (sleep ? 0x40 : 0x00));
+	spi.write<uint8_t>(REG::PWR_MGMT_1, 0x01 | (sleep ? 0x40 : 0x00));
 }
 
 void ICM20948::init()
 {
 	switchBank(0);
-	spi.write<uint8_t>(0x06, 0x80); // reset
+	spi.write<uint8_t>(REG::PWR_MGMT_1, 0x80); // reset
 	vTaskDelay(100 / portTICK_PERIOD_MS);
 
-	uint8_t id = spi.read<uint8_t>(0x00);
+	uint8_t id = spi.read<uint8_t>(REG::WHO_AM_I);
 	if (id != 0xea) {
 		ESP_LOGW(TAG, "ICM module reports wrong id %02hhx", id);
 	}
 
-	spi.write<uint8_t>(0x03, 0x30); // DMP disabled, fifo disabled, i2c master enabled, SPI mode.
+	spi.write<uint8_t>(REG::USER_CTRL, 0x30); // DMP disabled, fifo disabled, i2c master enabled, SPI mode.
 	setSleep(false); // power up the sensors
 
 }
