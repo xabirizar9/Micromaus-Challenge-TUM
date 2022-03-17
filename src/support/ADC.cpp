481c1a47f7f1f4cd1a1853d38fdb63e2415c1eca
Alexander Fuchs <alex.fu27@gmail.com>
2022-03-17 18:12:18 +0100

    deleted ADC2 which can't be used, adc reading now reports millivolts

--- src/support/ADC.cpp
+++ src/support/ADC.cpp
@@ -1,9 +1,11 @@
 #include "support/ADC.hpp"
 
 #include <driver/adc.h>
+#include <esp_adc_cal.h>
 #include <esp_log.h>
 #include <exception>
 
+static const char* TAG = "adc";
 static const adc_bits_width_t ADC1_BIT_WIDTH = ADC_WIDTH_BIT_13;
 static const adc_bits_width_t ADC2_BIT_WIDTH = ADC_WIDTH_BIT_13;
 
@@ -16,9 +18,43 @@ public:
 	}
 };
 
-ADC1::ADC1()
+ADC1::ADC1():
+	characteristics(new esp_adc_cal_characteristics_t())
 {
 	adc1_config_width(ADC1_BIT_WIDTH);
+	esp_err_t ok = esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_VREF);
+	if (ok == ESP_OK) {
+		ESP_LOGI(TAG, "ADC has VREF calibration data in eFuse");
+	} else if (ok == ESP_ERR_NOT_SUPPORTED) {
+		ESP_LOGW(TAG, "ADC does not have VREF calibration data in eFuse");
+	}
+	ok = esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_TP);
+	if (ok == ESP_OK) {
+		ESP_LOGI(TAG, "ADC has TP calibration data in eFuse");
+	} else if (ok == ESP_ERR_NOT_SUPPORTED) {
+		ESP_LOGW(TAG, "ADC does not have TP calibration data in eFuse");
+	}
+
+	esp_adc_cal_characteristics_t* c = reinterpret_cast<esp_adc_cal_characteristics_t*>(characteristics);
+	esp_adc_cal_value_t mode = esp_adc_cal_characterize(
+			ADC_UNIT_1, ADC_ATTEN_DB_11, ADC1_BIT_WIDTH,
+			1100, c);
+	if (mode == ESP_ADC_CAL_VAL_EFUSE_VREF) {
+		ESP_LOGI(TAG, "ADC1 characterized by eFuse VREF");
+	} else if (mode == ESP_ADC_CAL_VAL_EFUSE_TP) {
+		ESP_LOGI(TAG, "ADC1 characterized by eFuse TP");
+	} else if (mode == ESP_ADC_CAL_VAL_DEFAULT_VREF) {
+		ESP_LOGI(TAG, "ADC1 characterized by default VREF");
+	}
+
+	ESP_LOGI(TAG, "ADC1 characteristics: a = %u, b = %u, vref=%umV data %p %p",
+			c->coeff_a, c->coeff_b, c->vref, c->low_curve, c->high_curve);
+
+}
+
+ADC1::~ADC1()
+{
+	delete reinterpret_cast<esp_adc_cal_characteristics_t*>(characteristics);
 }
 
 static adc1_channel_t adc1GetChannelByGPIONumber(uint8_t n)
@@ -46,7 +82,14 @@ ADCChannel ADC1::configureChannel(adc1_channel_t channel)
 
 int ADC1::read(uint8_t ch)
 {
-	return adc1_get_raw((adc1_channel_t)ch);
+
+	int v = adc1_get_raw((adc1_channel_t)ch);
+	if (v < 0) // error value
+		return v;
+
+	uint32_t mVolts = esp_adc_cal_raw_to_voltage(v,
+			reinterpret_cast<esp_adc_cal_characteristics_t*>(characteristics));
+	return mVolts;
 }
 
 int ADCChannel::read()
