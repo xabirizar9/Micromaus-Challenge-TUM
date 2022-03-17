29a19c0849af3eaba5c0ba8fc63f92f4dded9ddc
Alexander Fuchs <alex.fu27@gmail.com>
2022-03-17 16:42:29 +0100

    needed to get over adc code from Sensors_tests

--- include/support/ADC.hpp
+++ include/support/ADC.hpp
@@ -1,40 +1,77 @@
 #pragma once
 
+#include <cstdint>
 #include <driver/adc.h>
 
-#include <cstdint>
-
+class ADC;
 class ADC1;
+class ADC2;
 
-class ADCChannel {
-   public:
+class ADCChannel
+{
+public:
 	/// read the channel
 	int read();
 
-   private:
-	ADCChannel(ADC1& adc, adc1_channel_t channel) : adc(adc), channel(channel) {}
+private:
+	ADCChannel(ADC &adc, uint8_t channel) : adc(adc), channel(channel) {}
 
-	ADC1& adc;
-	adc1_channel_t channel;
+	ADC &adc;
+	uint8_t channel;
 
 	friend class ADC1;
+	friend class ADC2;
 };
 
-class ADC1 {
-   public:
+class ADC
+{
+public:
+	/// configure a channel for reading
+	virtual ADCChannel configurePin(uint8_t pin) = 0;
+
+private:
+	ADC() = default;
+
+	virtual int read(uint8_t) = 0;
+	friend class ADCChannel;
+	friend class ADC1;
+	friend class ADC2;
+};
+
+class ADC1 : public ADC
+{
+public:
 	/// return singleton instance
-	static ADC1& instance() {
+	static ADC1 &instance()
+	{
 		static ADC1 inst;
 		return inst;
 	};
 
 	/// configure a channel for reading
 	ADCChannel configureChannel(adc1_channel_t channel);
-	ADCChannel configureChannel(uint8_t pin);
+	virtual ADCChannel configurePin(uint8_t pin) override;
 
-   private:
+private:
 	ADC1();
-	int read(adc1_channel_t);
+	virtual int read(uint8_t) override;
+};
 
-	friend class ADCChannel;
+class ADC2 : public ADC
+{
+public:
+	/// return singleton instance
+	static ADC2 &instance()
+	{
+		static ADC2 inst;
+		return inst;
+	};
+
+	/// configure a channel for reading
+	ADCChannel configureChannel(adc2_channel_t channel);
+	virtual ADCChannel configurePin(uint8_t pin) override;
+
+private:
+	ADC2();
+	virtual int read(uint8_t) override;
 };
