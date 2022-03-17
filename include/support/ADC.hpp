4662c9bb042f4ceb10dda4a752eb3052e776b139
Alexander Fuchs <alex.fu27@gmail.com>
2022-03-17 18:12:18 +0100

    deleted ADC2 which can't be used, adc reading now reports millivolts

--- include/support/ADC.hpp
+++ include/support/ADC.hpp
@@ -3,42 +3,24 @@
 #include <cstdint>
 #include <driver/adc.h>
 
-class ADC;
 class ADC1;
-class ADC2;
 
 class ADCChannel
 {
 public:
-	/// read the channel
+	/// read the channel in millivolts, returns negative value on error
 	int read();
 
 private:
-	ADCChannel(ADC &adc, uint8_t channel) : adc(adc), channel(channel) {}
+	ADCChannel(ADC1 &adc, uint8_t channel) : adc(adc), channel(channel) {}
 
-	ADC &adc;
+	ADC1 &adc;
 	uint8_t channel;
 
 	friend class ADC1;
-	friend class ADC2;
 };
 
-class ADC
-{
-public:
-	/// configure a channel for reading
-	virtual ADCChannel configurePin(uint8_t pin) = 0;
-
-private:
-	ADC() = default;
-
-	virtual int read(uint8_t) = 0;
-	friend class ADCChannel;
-	friend class ADC1;
-	friend class ADC2;
-};
-
-class ADC1 : public ADC
+class ADC1
 {
 public:
 	/// return singleton instance
@@ -50,28 +32,14 @@ public:
 
 	/// configure a channel for reading
 	ADCChannel configureChannel(adc1_channel_t channel);
-	virtual ADCChannel configurePin(uint8_t pin) override;
+	ADCChannel configurePin(uint8_t pin);
 
 private:
 	ADC1();
-	virtual int read(uint8_t) override;
+	~ADC1();
+	int read(uint8_t);
+	void* characteristics;
+
+	friend class ADCChannel;
 };
 
-class ADC2 : public ADC
-{
-public:
-	/// return singleton instance
-	static ADC2 &instance()
-	{
-		static ADC2 inst;
-		return inst;
-	};
-
-	/// configure a channel for reading
-	ADCChannel configureChannel(adc2_channel_t channel);
-	virtual ADCChannel configurePin(uint8_t pin) override;
-
-private:
-	ADC2();
-	virtual int read(uint8_t) override;
-};
