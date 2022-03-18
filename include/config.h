7a5db896dfaeacd2af25b471472a6ce198609083
Alexander Fuchs <alex.fu27@gmail.com>
2022-03-18 17:19:57 +0100

    gpio numbers for SPI

--- include/config.h
+++ include/config.h
@@ -1,7 +1,7 @@
 
 #ifdef ESP32_S2_MAUS
 
-struct IO { // gpio numbers
+namespace IO { // gpio numbers
 	static constexpr uint8_t LED_DEBUG[4] {4, 3, 2, 1};
 
 	static constexpr uint8_t BUTTON = 8;
@@ -27,7 +27,14 @@ struct IO { // gpio numbers
 
 	static constexpr Motor MOTOR_L {7, 19, 20, {21, 26}};
 	static constexpr Motor MOTOR_R {13, 14, 15, {16, 17}};
-};
+
+	namespace SPI {
+		static constexpr uint8_t CS_INS = 34;
+		static constexpr uint8_t MOSI = 35;
+		static constexpr uint8_t CLK = 36;
+		static constexpr uint8_t MISO = 37;
+	}
+}
 
 #else
 #error "no board defined"
