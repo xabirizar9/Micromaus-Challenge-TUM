a19ac3096d57ad31c3c2bdb90ca1f063c8381be2
Xabier Irizar <xabier.irizar@tum.de>
2022-03-22 13:25:00 +0100

    reordered hpp file

--- include/Navigation.hpp
+++ include/Navigation.hpp
@@ -2,25 +2,12 @@
 
 #include "Controller.hpp"
 // Check if there's a wall on the left
-void check_left(){
-
-}
-void check_right(){
-
-}
-void check_front(){
-
-}
+void check_left(uint16_t leftDistance);
+void check_right(uint16_t rightDistance);
+void check_front(uint16_t frontDistance);
 
 /* Function should return an empty grid, where each
 grid element has an occupancy value.  */
-void generate_grid(int& width, int& height){
-	
-}
+void generate_grid(int& width, int& height);
 
-void
-
-
-class PathNavigation {
-	void begin();
-};
+class PathNavigation;
