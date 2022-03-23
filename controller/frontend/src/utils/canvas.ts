ba2bcc258410e284a0a42f53cb52765f2be41db9
wlawlawlad <wlad.meixner@tum.de>
2022-03-24 00:43:42 +0100

    feat: update rendering

--- controller/frontend/src/utils/canvas.ts
+++ controller/frontend/src/utils/canvas.ts
@@ -1,7 +1,8 @@
-// copied from https://stackoverflow.com/questions/1255512/how-to-draw-a-rounded-rectangle-using-html-canvas
+// copied from
+// https://stackoverflow.com/questions/1255512/how-to-draw-a-rounded-rectangle-using-html-canvas
 
-import { Size } from "./geo";
-import { getGlobalStyle } from "./style";
+import {Size} from './geo';
+import {getGlobalStyle} from './style';
 
 /**
  * Draws a rounded rectangle using the current state of the canvas.
@@ -22,58 +23,48 @@ import { getGlobalStyle } from "./style";
  * @param {Boolean} [stroke = true] Whether to stroke the rectangle.
  */
 function roundRect(ctx, x, y, width, height, radius, fill, stroke) {
-  if (typeof stroke === "undefined") {
-    stroke = true;
-  }
-  if (typeof radius === "undefined") {
-    radius = 5;
-  }
-  if (typeof radius === "number") {
-    radius = { tl: radius, tr: radius, br: radius, bl: radius };
-  } else {
-    var defaultRadius = { tl: 0, tr: 0, br: 0, bl: 0 };
-    for (var side in defaultRadius) {
-      radius[side] = radius[side] || defaultRadius[side];
-    }
-  }
-  ctx.beginPath();
-  ctx.moveTo(x + radius.tl, y);
-  ctx.lineTo(x + width - radius.tr, y);
-  ctx.quadraticCurveTo(x + width, y, x + width, y + radius.tr);
-  ctx.lineTo(x + width, y + height - radius.br);
-  ctx.quadraticCurveTo(
-    x + width,
-    y + height,
-    x + width - radius.br,
-    y + height
-  );
-  ctx.lineTo(x + radius.bl, y + height);
-  ctx.quadraticCurveTo(x, y + height, x, y + height - radius.bl);
-  ctx.lineTo(x, y + radius.tl);
-  ctx.quadraticCurveTo(x, y, x + radius.tl, y);
-  ctx.closePath();
-  if (fill) {
-    ctx.fill();
-  }
-  if (stroke) {
-    ctx.stroke();
-  }
+	if (typeof stroke === 'undefined') {
+		stroke = true;
+	}
+	if (typeof radius === 'undefined') {
+		radius = 5;
+	}
+	if (typeof radius === 'number') {
+		radius = {tl: radius, tr: radius, br: radius, bl: radius};
+	} else {
+		var defaultRadius = {tl: 0, tr: 0, br: 0, bl: 0};
+		for (var side in defaultRadius) {
+			radius[side] = radius[side] || defaultRadius[side];
+		}
+	}
+	ctx.beginPath();
+	ctx.moveTo(x + radius.tl, y);
+	ctx.lineTo(x + width - radius.tr, y);
+	ctx.quadraticCurveTo(x + width, y, x + width, y + radius.tr);
+	ctx.lineTo(x + width, y + height - radius.br);
+	ctx.quadraticCurveTo(x + width, y + height, x + width - radius.br, y + height);
+	ctx.lineTo(x + radius.bl, y + height);
+	ctx.quadraticCurveTo(x, y + height, x, y + height - radius.bl);
+	ctx.lineTo(x, y + radius.tl);
+	ctx.quadraticCurveTo(x, y, x + radius.tl, y);
+	ctx.closePath();
+	if (fill) {
+		ctx.fill();
+	}
+	if (stroke) {
+		ctx.stroke();
+	}
 }
 
 export function drawGrid(
-  ctx: CanvasRenderingContext2D,
-  width: number,
-  height: number,
-  gap: number,
-  mazeSize: Size
-) {
-  for (let i = 0; i < mazeSize.width; i++) {
-    for (let j = 0; j < mazeSize.height; j++) {
-      const y = gap + (height + gap) * i;
-      const x = gap + (width + gap) * j;
-      console.log(getGlobalStyle("--main-bg-secondary"));
-      ctx.fillStyle = "#cccccc";
-      roundRect(ctx, x, y, width, height, 15, true, false);
-    }
-  }
+	ctx: CanvasRenderingContext2D, width: number, height: number, gap: number, mazeSize: Size) {
+	for (let i = 0; i < mazeSize.width; i++) {
+		for (let j = 0; j < mazeSize.height; j++) {
+			const y = gap + (height + gap) * i;
+			const x = gap + (width + gap) * j;
+			console.log(getGlobalStyle('--main-bg-secondary'));
+			ctx.fillStyle = '#cccccc';
+			roundRect(ctx, x, y, width, height, 15, true, false);
+		}
+	}
 }
--- controller/frontend/src/utils/geo.ts
+++ controller/frontend/src/utils/geo.ts
@@ -1,14 +1,12 @@
 export type Vector2D = {
-  x: number;
-  y: number;
+	x: number; y: number;
 };
 
 export type Size = {
-  width: number;
-  height: number;
+	width: number; height: number;
 };
 
 export const mazeSize: Size = {
-  width: 6,
-  height: 6,
+	width: 6,
+	height: 6,
 };
