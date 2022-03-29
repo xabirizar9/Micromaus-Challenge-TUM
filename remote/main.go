1fa9683d77815fa6a892099f98a6a0465d7dd71a
wlawlawlad <wlad.meixner@tum.de>
2022-03-29 23:13:55 +0200

    feat: add remote discovery

--- remote/main.go
+++ remote/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net"
+	"net/http"
+
+	"go.uber.org/zap"
 )
 
 var (
 	robotIP net.IP
+	log, _  = zap.NewDevelopment()
 )
 
 func handleUDPConnection(conn *net.UDPConn) {
@@ -15,19 +17,20 @@ func handleUDPConnection(conn *net.UDPConn) {
 
 	buffer := make([]byte, 1024)
 
-	n, addr, err := conn.ReadFromUDP(buffer)
+	n, _, err := conn.ReadFromUDP(buffer)
 
 	if err != nil {
-		log.Fatal(err)
+		log.Error("failed to read maus IP", zap.Error(err))
+		return
 	}
 
 	if n != 4 {
-		fmt.Println("Invalid UDP IP packet")
+		log.Error("invalid maus address length", zap.Int("length", n))
 		return
 	}
 
 	robotIP = (net.IP(buffer[:4]))
-	fmt.Println(robotIP.String())
+	log.Info("got maus IP", zap.String("ip", robotIP.String()))
 }
 
 func main() {
@@ -38,19 +41,39 @@ func main() {
 	udpAddr, err := net.ResolveUDPAddr("udp4", service)
 
 	if err != nil {
-		log.Fatal(err)
+		log.Error("failed to open UDP steam", zap.Error(err))
+		return
 	}
 
 	// setup listener for incoming UDP connection
 	ln, err := net.ListenUDP("udp", udpAddr)
 
 	if err != nil {
-		log.Fatal(err)
+		log.Error("failed to open UDP steam", zap.Error(err))
+		return
 	}
 
-	fmt.Println("UDP server up and listening on port 8888")
+	log.Info("UDP server up and listening on port 8888")
 
 	defer ln.Close()
+
+	go func() {
+
+		// web socket handler
+		http.HandleFunc("/maus", func(w http.ResponseWriter, r *http.Request) {
+			l := log.With(
+				zap.String("url", r.URL.String()),
+				zap.String("ip", r.RemoteAddr),
+			)
+			l.Info("maus requested")
+			w.Write([]byte(robotIP.String()))
+			w.WriteHeader(http.StatusOK)
+		})
+
+		log.Info("server started")
+
+		http.ListenAndServe(":7777", nil)
+	}()
 
 	for {
 		// wait for UDP client to connect
