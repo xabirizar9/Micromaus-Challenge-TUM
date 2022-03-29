b6ccb4490310f8113afd630d5d4f61ac6b284364
wlawlawlad <wlad.meixner@tum.de>
2022-03-29 21:16:58 +0200

    feat: add UDP receiver server

---
+++ remote/go.mod
@@ -0,0 +1,3 @@
+module gitlab.lrz.de/waxn/micromaus-server
+
+go 1.17
---
+++ remote/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+var (
+	robotIP net.IP
+)
+
+func handleUDPConnection(conn *net.UDPConn) {
+	// here is where you want to do stuff like read or write to client
+
+	buffer := make([]byte, 1024)
+
+	n, addr, err := conn.ReadFromUDP(buffer)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if n != 4 {
+		fmt.Println("Invalid UDP IP packet")
+		return
+	}
+
+	robotIP = (net.IP(buffer[:4]))
+
+	fmt.Println(robotIP.String())
+}
+
+func main() {
+	hostName := "0.0.0.0"
+	portNum := "8888"
+	service := hostName + ":" + portNum
+
+	udpAddr, err := net.ResolveUDPAddr("udp4", service)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// setup listener for incoming UDP connection
+	ln, err := net.ListenUDP("udp", udpAddr)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("UDP server up and listening on port 8888")
+
+	defer ln.Close()
+
+	for {
+		// wait for UDP client to connect
+		handleUDPConnection(ln)
+	}
+
+}
