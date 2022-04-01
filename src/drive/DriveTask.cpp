95c0a6c2c63fad7bde57f93c6ac5dbe8e5d39ed3
Niklas Günther <niklas.guenther@tum.de>
2022-04-01 14:01:09 +0200

    temp

--- src/drive/DriveTask.cpp
+++ src/drive/DriveTask.cpp
@@ -4,6 +4,7 @@
 #include "MazeExplorer.hpp"
 #include "freertos/FreeRTOS.h"
 #include "freertos/queue.h"
+#include "message.pb.h"
 #include "periph/Motor.hpp"
 
 void driveTask(void *arg) {
@@ -14,11 +15,9 @@ void driveTask(void *arg) {
 	QueueHandle_t execQueue = driver->executionQueue;
 
 	DriveCommand cmd;
-	DriveCommand *curCmd;
+	DriveCommand *curCmd
 
-	bool working = false;
-
-	while (true) {
+		while (true) {
 		// update and get state
 		controller->updateSensors();
 		controller->updatePosition();
@@ -33,12 +32,43 @@ void driveTask(void *arg) {
 			}
 		}
 
-		// switch (cmd.type) {
-		// 	case DriveCommandType::move: break;
-		// }
+		switch (cmd.type) {
+			case DriveCommandType::move: break;
+			case DriveCommandType::moveCells: {
+				_MsgTurn payload = NavigationPacket data = controller.getstate();
+				float encoderPos->data.speed;
+				float speed->data.speed;
+				controller.drive();
+				break
+			};
+			case DriveCommandType::turnAround: break;
+			case DriveCommandType::turnLeft: break;
+			case DriveCommandType::turnRight: break;
+			case DriveCommandType::turnLeftOnSpot: break;
+			case DriveCommandType::turnRightOnSpot: break;
+		}
 
 		curCmd = NULL;
 
 		vTaskDelay(pdMS_TO_TICKS(navInterval));
 	}
+}
+
+void turn() {
+	// vTaskDelay(pdMS_TO_TICKS(5000));
+	int8_t pre = -1;
+	if (degree < 0) {
+		pre = 1;
+	}
+	uint8_t rMaus = 60;
+	float dRad = abs(degree) * rMaus;
+	float duration = dRad / speed;
+
+	this->leftSpeedTickTarget = convertMMsToTPS(-pre * speed);
+	this->rightSpeedTickTarget = convertMMsToTPS(pre * speed);
+
+	ESP_LOGI(TAG, "no time passed %f %f", leftSpeedTickTarget, rightSpeedTickTarget);
+
+	vTaskDelay(pdMS_TO_TICKS(duration));
+	this->setSpeed(0);
 }
