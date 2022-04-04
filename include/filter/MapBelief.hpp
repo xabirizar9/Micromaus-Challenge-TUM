46270b28e6b9d0f2be3196fa63117b81fb77c930
Alexander Fuchs <alex.fu27@gmail.com>
2022-04-04 17:40:09 +0200

    refactor: MapBelief has more complex implementations now in MapBelief.cpp

--- include/filter/MapBelief.hpp
+++ include/filter/MapBelief.hpp
@@ -3,11 +3,6 @@
 #include <stdexcept>
 #include <vector>
 
-#ifdef MAPBELIEF_TEST
-#include <iostream>
-using namespace std;
-#endif
-
 #include "nav/Position.hpp"
 
 /**
@@ -35,25 +30,7 @@ class MapBelief {
 	/**
 	 * prior: prior probability that a wall is set
 	 */
-	MapBelief(int width, int height, const float prior)
-		: width(width),
-		  height(height),
-		  probabilities((width + 1) * (height + 1) * 2, prior),
-		  fixed((width + 1) * (height + 1) * 2, false) {
-		// set all border walls to fixed
-		// at() performs bounds checking so we immediately see if i fucked up
-		for (int x = 0; x <= width; ++x) {
-			fixed.at(locate(x, 0, nav::CardinalDirection::SOUTH)) = true;
-			fixed.at(locate(x, height, nav::CardinalDirection::SOUTH)) = true;
-			fixed.at(locate(x, height, nav::CardinalDirection::WEST)) = true;
-		}
-		for (int y = 0; y <= height; ++y) {
-			fixed.at(locate(0, y, nav::CardinalDirection::WEST)) = true;
-			fixed.at(locate(width, y, nav::CardinalDirection::WEST)) = true;
-			fixed.at(locate(width, y, nav::CardinalDirection::SOUTH)) = true;
-		}
-	}
-
+	MapBelief(int width, int height, const float prior);
 	~MapBelief() = default;
 
 	/// I forbid copying for performance reasons, hoping that i will
@@ -68,65 +45,21 @@ class MapBelief {
 	 * Throws std::invalid_argument when a wall outside the field is
 	 * accessed.
 	 */
-	void set(int x, int y, nav::CardinalDirection d, float f) {
-		if ((x < 0) || (x >= width) || (y < 0) || (y >= height)) {
-			throw std::invalid_argument("MapBelief write access out of bounds");
-		}
-
-		int loc = locate(x, y, d);
-
-		probabilities[loc] = f;
-	}
+	void set(int x, int y, nav::CardinalDirection d, float f);
 
 	/**
 	 * Get the probability that the wall at (x, y, d) is placed.
 	 * Walls outside the field are always regarded as placed,
 	 * so if accessing a coordinate outside the field, returns 1.0.
 	 */
-	float get(int x, int y, nav::CardinalDirection d) const {
-		if ((x < 0) || (x >= width) || (y < 0) || (y >= height)) {
-			return 1.0;	 // all walls outside the field are set
-		}
-
-		int loc = locate(x, y, d);
-
-		if (fixed[loc])
-			return 1.0;
-
-		return probabilities[loc];
-	}
+	float get(int x, int y, nav::CardinalDirection d) const;
 
 	void setFixed(int x, int y, nav::CardinalDirection d, bool fix) {
 		fixed.at(locate(x, y, d)) = fix;
 	}
 
-#ifdef MAPBELIEF_TEST
-	void print() const {
-		for (int y = height; y >= 0; y--) {
-			// west walls
-			for (int x = 0; x <= width; x++) {
-				int loc = locate(x, y, nav::CardinalDirection::WEST);
-				if (fixed[loc]) {
-					cout << "\033[0;31m";
-				} else {
-					cout << "\033[0m";
-				}
-				cout << "|  ";
-			}
-			cout << endl;
-			// south walls
-			for (int x = 0; x <= width; x++) {
-				int loc = locate(x, y, nav::CardinalDirection::SOUTH);
-				if (fixed[loc])
-					cout << "\033[0;31m";
-				else
-					cout << "\033[0m";
-				cout << " --";
-			}
-			cout << endl;
-		}
-	}
-#endif
+	/// not implemented for ESP
+	void print() const;
 
    private:
 	/// transform x/y/d coordinate to array index.
@@ -137,7 +70,8 @@ class MapBelief {
 		if (d == nav::CardinalDirection::NORTH) {
 			return locate(x, y + 1, nav::CardinalDirection::SOUTH);
 		}
-		return x * (width + 1) + y + ((d == nav::CardinalDirection::WEST) ? (width + 1) * (height + 1) : 0);
+		return x * (width + 1) + y +
+			   ((d == nav::CardinalDirection::WEST) ? (width + 1) * (height + 1) : 0);
 	}
 
 	const int width;
@@ -145,11 +79,3 @@ class MapBelief {
 	std::vector<float> probabilities;
 	std::vector<bool> fixed;
 };
-
-#ifdef MAPBELIEF_TEST
-int main() {
-	MapBelief bel(6, 6, 0.5);
-	bel.print();
-	return 0;
-}
-#endif
