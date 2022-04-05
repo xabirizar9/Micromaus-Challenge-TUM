8b2ad631a76d1429e6c2ac46585818e2930a1696
Alexander Fuchs <al.fuchs@tum.de>
2022-04-05 12:30:07 +0200

    Vec: now has Row number template argument

--- include/support/Linalg.hpp
+++ include/support/Linalg.hpp
@@ -1,53 +1,56 @@
 #pragma once
 
+#include <algorithm>
 #include <array>
 #include <cstdint>
 #include <type_traits>
 
-namespace linalg {
+namespace la {
 
-template <typename T>
-struct Vec {
-	union {
-		std::array<T, 3> buffer;
-		struct {
-			T x;
-			T y;
-			T z;
-		};
-	};
+/**
+ * Vector class
+ * T: number type
+ * R: number of Rows
+ */
+template <typename T, unsigned int R>
+struct Vec : public std::array<T, R> {
+	Vec() : std::array<T, R>() {}
+	Vec(std::array<T, R> l) : std::array<T, R>(l) {}
 };
 
-template <typename T, typename S>
-Vec<std::common_type_t<T, S>> operator*(const Vec<T>& v, const S& t) {
-	return {v.x * t, v.y * t, v.z * t};
+template <typename T, typename S, unsigned int R>
+Vec<std::common_type_t<T, S>, R> operator*(const Vec<T, R>& v, const S& t) {
+	Vec<std::common_type_t<T, S>, R> out;
+	std::transform(v.cbegin(), v.cend(), out.begin(), [t](T in) { return in * t; });
+	return std::move(out);
 }
 
-template <typename T, typename S>
-Vec<std::common_type_t<T, S>> operator*(const S& t, const Vec<T>& v) {
+template <typename T, typename S, unsigned int R>
+Vec<std::common_type_t<T, S>, R> operator*(const S& t, const Vec<T, R>& v) {
 	return v * t;
 }
 
-template <typename T, typename S>
-Vec<std::common_type_t<T, S>> operator+(const Vec<T>& v, const Vec<S>& w) {
-	return {v.x + w.x, v.y + w.y, v.z + w.z};
+template <typename T, typename S, unsigned int R>
+Vec<std::common_type_t<T, S>, R> operator+(const Vec<T, R>& v, const Vec<S, R>& w) {
+	Vec<std::common_type_t<T, S>, R> out;
+	for (unsigned int i = 0; i < R; ++i)
+		out[i] = v[i] + w[i];
+	return std::move(out);
 }
 
-template <typename T, typename S>
-Vec<std::common_type_t<T, S>> operator-(const Vec<T>& v, const Vec<S>& w) {
-	return {v.x - w.x, v.y - w.y, v.z - w.z};
+template <typename T, typename S, unsigned int R>
+Vec<std::common_type_t<T, S>, R> operator-(const Vec<T, R>& v, const Vec<S, R>& w) {
+	Vec<std::common_type_t<T, S>, R> out;
+	for (unsigned int i = 0; i < R; ++i)
+		out[i] = v[i] - w[i];
+	return std::move(out);
 }
 
-template <typename T, typename S>
-Vec<std::common_type_t<T, S>> operator/(const Vec<T>& v, const S& t) {
-	return {v.x / t, v.y / t, v.z / t};
+template <typename T, typename S, unsigned int R>
+Vec<std::common_type_t<T, S>, R> operator/(const Vec<T, R>& v, const S& t) {
+	Vec<std::common_type_t<T, S>, R> out;
+	std::transform(v.cbegin(), v.cend(), out.begin(), [t](T in) { return in / t; });
+	return std::move(out);
 }
 
-}  // namespace linalg
-
-/*
-template<typename T>
-linalg::Vec<T> operator+(const linalg::Vec<T>& v, const linalg::Vec<T>& w) {
-	return {v.x + w.x, v.y + w.y, v.z + w.z};
-}
-*/
+}  // namespace la
