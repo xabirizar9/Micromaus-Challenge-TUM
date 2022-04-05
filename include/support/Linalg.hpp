c8c50cc70db208176e11ea09c9fe93d727484598
Alexander Fuchs <al.fuchs@tum.de>
2022-04-05 13:55:21 +0200

    Vec is now a kind of Mat

--- include/support/Linalg.hpp
+++ include/support/Linalg.hpp
@@ -7,49 +7,70 @@
 
 namespace la {
 
-/**
- * Vector class
- * T: number type
- * R: number of Rows
- */
-template <typename T, unsigned int R>
-struct Vec : public std::array<T, R> {
-	Vec() : std::array<T, R>() {}
-	Vec(std::array<T, R> l) : std::array<T, R>(l) {}
+template <typename T, unsigned int R, unsigned int C>
+struct Mat {
+	std::array<T, R * C> data;
+
+	constexpr Mat() : data() {}
+	constexpr Mat(const std::array<T, R * C>& l) : data(l) {}
+
+	T& at(unsigned int r, unsigned int c) {
+		return data.at(r * C + c);
+	}
+
+	const T& at(unsigned int r, unsigned int c) const {
+		return data.at(r * C + c);
+	}
 };
 
-template <typename T, typename S, unsigned int R>
-Vec<std::common_type_t<T, S>, R> operator*(const Vec<T, R>& v, const S& t) {
-	Vec<std::common_type_t<T, S>, R> out;
-	std::transform(v.cbegin(), v.cend(), out.begin(), [t](T in) { return in * t; });
+template <typename T, unsigned int R>
+struct Vec : public Mat<T, R, 1> {
+	using Mat<T, R, 1>::Mat;
+
+	constexpr Vec(const Mat<T, R, 1>& m) : Mat<T, R, 1>(m.data) {}
+
+	T& at(unsigned int r) {
+		return Mat<T, R, 1>::at(r, 1);
+	}
+
+	const T& at(unsigned int r) const {
+		return Mat<T, R, 1>::at(r, 1);
+	}
+};
+
+// Matrix-Scalar operations
+template <typename T, typename S, unsigned int R, unsigned int C>
+Mat<std::common_type_t<T, S>, R, C> operator*(const Mat<T, R, C>& v, const S& t) {
+	Mat<std::common_type_t<T, S>, R, C> out;
+	std::transform(v.data.cbegin(), v.data.cend(), out.data.begin(), [t](T in) { return in * t; });
 	return std::move(out);
 }
 
-template <typename T, typename S, unsigned int R>
-Vec<std::common_type_t<T, S>, R> operator*(const S& t, const Vec<T, R>& v) {
+template <typename T, typename S, unsigned int R, unsigned int C>
+Mat<std::common_type_t<T, S>, R, C> operator*(const S& t, const Mat<T, R, C>& v) {
 	return v * t;
 }
 
-template <typename T, typename S, unsigned int R>
-Vec<std::common_type_t<T, S>, R> operator+(const Vec<T, R>& v, const Vec<S, R>& w) {
-	Vec<std::common_type_t<T, S>, R> out;
-	for (unsigned int i = 0; i < R; ++i)
-		out[i] = v[i] + w[i];
+template <typename T, typename S, unsigned int R, unsigned int C>
+Mat<std::common_type_t<T, S>, R, C> operator+(const Mat<T, R, C>& v, const Mat<S, R, C>& w) {
+	Mat<std::common_type_t<T, S>, R, C> out;
+	for (unsigned int i = 0; i < R * C; ++i)
+		out.data[i] = v.data[i] + w.data[i];
 	return std::move(out);
 }
 
-template <typename T, typename S, unsigned int R>
-Vec<std::common_type_t<T, S>, R> operator-(const Vec<T, R>& v, const Vec<S, R>& w) {
-	Vec<std::common_type_t<T, S>, R> out;
-	for (unsigned int i = 0; i < R; ++i)
-		out[i] = v[i] - w[i];
+template <typename T, typename S, unsigned int R, unsigned int C>
+Mat<std::common_type_t<T, S>, R, C> operator-(const Mat<T, R, C>& v, const Mat<S, R, C>& w) {
+	Mat<std::common_type_t<T, S>, R, C> out;
+	for (unsigned int i = 0; i < R * C; ++i)
+		out.data[i] = v.data[i] - w.data[i];
 	return std::move(out);
 }
 
-template <typename T, typename S, unsigned int R>
-Vec<std::common_type_t<T, S>, R> operator/(const Vec<T, R>& v, const S& t) {
-	Vec<std::common_type_t<T, S>, R> out;
-	std::transform(v.cbegin(), v.cend(), out.begin(), [t](T in) { return in / t; });
+template <typename T, typename S, unsigned int R, unsigned int C>
+Mat<std::common_type_t<T, S>, R, C> operator/(const Mat<T, R, C>& v, const S& t) {
+	Mat<std::common_type_t<T, S>, R, C> out;
+	std::transform(v.data.cbegin(), v.data.cend(), out.data.begin(), [t](T in) { return in / t; });
 	return std::move(out);
 }
 
