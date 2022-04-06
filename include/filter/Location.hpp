a5d0aa52ec25b4916179ac23c39906509247a24b
Alexander Fuchs <al.fuchs@tum.de>
2022-04-06 09:17:37 +0200

    Location header now uses RobotPositionDistribution class

--- include/filter/Location.hpp
+++ include/filter/Location.hpp
@@ -1,17 +1,24 @@
 #pragma once
 
+#include "filter/RobotPositionDistribution.hpp"
 /**
  * TODO: this must be replaced with a real struct containing the sensor data.
  */
 class SensorState;
 
-class RobotPositionDistribution;
-
 class Location {
+	/**
+	 * initializes the robot position to the center of the (0, 0) field,
+	 * which is (9cm, 9cm, EAST) and a std-derivation of (9cm, 9cm, 45deg)
+	 */
+	Location();
+
 	/**
 	 * Initialize the distribution of the robot position to the given one.
 	 */
-	void setDistribution(const RobotPositionDistribution&);
+	void setDistribution(const RobotPositionDistribution& n) {
+		rdp = n;
+	}
 
 	/**
 	 * Predicts the location of the robot given the desired wheel velocities
