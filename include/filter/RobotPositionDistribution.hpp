3a7481193fe04b16832b84dfb9eb781b862ef762
Alexander Fuchs <al.fuchs@tum.de>
2022-04-06 09:17:09 +0200

    added RobotPositionDistribution class for gaussian robot positions

--- include/filter/RobotPositionDistribution.hpp
+++ include/filter/RobotPositionDistribution.hpp
@@ -1,3 +1,18 @@
 #pragma once
 
-class RobotPositionDistribution {};
+#include "support/Linalg.hpp"
+
+/**
+ * Gaussian distribution of robot position.
+ */
+struct RobotPositionDistribution {
+	la::Vec2f positionMean;
+	float thetaMean;
+	la::Vec2f velocityMean;
+	float omegaMean;
+
+	la::Mat<float, 2, 2> positionStd;
+	float thetaStd;
+	la::Mat<float, 2, 2> velocityStd;
+	float omegaStd;
+};
